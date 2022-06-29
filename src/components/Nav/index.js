10767143dd77139396be0c11ae303cebf9ae6b81
Thuy Nguyen <ttruong06@gmail.com>
2022-06-28 20:27:03 -0400

    finished Nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,20 +1,28 @@
 import React from "react";
 
-const categories = [
-  {
-    name: "commercial",
-    description:
-      "Photos of grocery stores, food trucks, and other commercial projects",
-  },
-  { name: "portraits", description: "Portraits of people in my life" },
-  { name: "food", description: "Delicious delicacies" },
-  {
-    name: "landscape",
-    description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-  },
-];
+
 
 function Nav() {
+
+    function categorySelected(name) {
+        console.log(`${name} clicked`)
+      }
+      
+    const categories = [
+        {
+          name: "commercial",
+          description:
+            "Photos of grocery stores, food trucks, and other commercial projects",
+        },
+        { name: "portraits", description: "Portraits of people in my life" },
+        { name: "food", description: "Delicious delicacies" },
+        {
+          name: "landscape",
+          description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+        },
+      ];
+
+
   return (
     <header>
       <h2>
@@ -34,9 +42,7 @@ function Nav() {
             <span>Contact</span>
           </li>
           {categories.map((category) => (
-            <li className="mx-1" key={category.name}>
-              <span> {category.name}</span>
-            </li>
+            <li className="mx-1" key={category.name}><span onClick={categorySelected}> {category.name } </span></li>
           ))}
         </ul>
       </nav>
