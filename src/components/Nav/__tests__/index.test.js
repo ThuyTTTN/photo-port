6759925419d86f040bfa92101b5e3c3b93836359
Thuy Nguyen <ttruong06@gmail.com>
2022-06-30 14:59:36 -0400

    created index.test for gallergy and photolist

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -4,6 +4,21 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+//Add categories array so we can handle props for Nav.
+//Declare props
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life'}
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
+// it('renders', () => {
+//     render(<Nav
+//         categories = {categories}
+//         setCurrentCategory={mockSetCurrentCategory}
+//         currentCategory={mockCurrentCategory} />);
+// })
+
 //cleanup statement
 afterEach(cleanup);
 
