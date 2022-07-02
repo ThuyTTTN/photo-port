98a8e8f3fc3a85ba6472099f1878a2558c5e7a16
Thuy Nguyen <ttruong06@gmail.com>
2022-07-02 16:01:04 -0400

    Model component

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPhoto, setCurrentPhoto] = useState();
 
   const [photos] = useState([
     {
@@ -104,20 +106,31 @@ const PhotoList = ({ category }) => {
   //going through each photo in the photos array to find every photo that matches the 'category' selected; if photo matches the condition, it is returned in an array and assigned to 'currentPhotos'
   const currentPhotos = photos.filter((photo) => photo.category === category);
 
+  const toggleModal = (image, i) => {
+    // current photo
+    setCurrentPhoto({...image, index: i});
+    setIsModalOpen(!isModalOpen);
+
+  }
+
+
   return (
     <div>
-      <Modal />
-      <div className="flex-row">
-        {currentPhotos.map((image, i) => (
-          <img
-            src={require(`../../assets/small/${category}/${i}.jpg`)}
-            alt={image.name}
-            className="img-thumbnail mx-1"
-            key={image.name}
-          />
-        ))}
-      </div>
+    {isModalOpen && (
+      <Modal onClose={toggleModal} currentPhoto={currentPhoto} />
+    )}
+    <div className="flex-row">
+      {currentPhotos.map((image, i) => (
+        <img
+          src={require(`../../assets/small/${category}/${i}.jpg`).default}
+          alt={image.name}
+          className="img-thumbnail mx-1"
+          onClick={() => toggleModal(image, i)}
+          key={image.name}
+        />
+      ))}
     </div>
+  </div>
   );
 };
 
